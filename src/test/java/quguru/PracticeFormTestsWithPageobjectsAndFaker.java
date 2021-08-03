67ab1086b1ded3f0edff886a19641473061828a9
Simon Postnikov <postnikov.sn@mail.ru>
2021-08-03 21:05:05 +0300

    testing jenkins

--- src/test/java/quguru/PracticeFormTestsWithPageobjectsAndFaker.java
+++ src/test/java/quguru/PracticeFormTestsWithPageobjectsAndFaker.java
@@ -20,7 +20,7 @@ public class PracticeFormTestsWithPageobjectsAndFaker extends TestBase {
 
     @BeforeAll
     static void setup() {
-     //   baseUrl = "https://demoqa.com";
+        //   baseUrl = "https://demoqa.com";
         startMaximized = true;
     }
 
@@ -44,89 +44,36 @@ public class PracticeFormTestsWithPageobjectsAndFaker extends TestBase {
         String city = "Noida";
 
 
-//        step("Open students registration form", () -> {
-//        registrationPage.openPage();});
-//        step("Fill students registration form", () -> {
-//        registrationPage.typeFirstName(firstName)
-//                .typeLastName(lastName)
-//                .typeEmail(email)
-//                .selectGender(gender)
-//                .typePhone(phone)
-//                .setDateOfBirth(day, month, year)
-//                .typeSubjects(subjects)
-//                .typeHobbies(hobbies)
-//                .uploudFile(picture)
-//                .typeAdress(adress)
-//                .selectState(state)
-//                .selectCity(city);
-//        registrationPage.clickButton();});
-//        step("Verify successful form submit", () -> {
-//        registrationPage.checkResultsTitle();
-//        registrationPage.checkResultsValue(firstName + " " + lastName)
-//                .checkResultsValue(email)
-//                .checkResultsValue(gender)
-//                .checkResultsValue(phone)
-//                .checkResultsValue(day + " " + month + "," + year)
-//                .checkResultsValue(subjects)
-//                .checkResultsValue(hobbies)
-//                .checkResultsValue(picture)
-//                .checkResultsValue(adress)
-//                .checkResultsValue(state + " " + city);});
         step("Open students registration form", () -> {
-            open("https://demoqa.com/automation-practice-form");
-            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            registrationPage.openPage();
         });
-
         step("Fill students registration form", () -> {
-            step("Fill common data", () -> {
-                $("#firstName").val(firstName);
-                $("#lastName").val(lastName);
-                $("#userEmail").val(email);
-                $("#genterWrapper").$(byText(gender)).click();
-                $("#userNumber").val(phone);
-            });
-            step("Set date", () -> {
-                $("#dateOfBirthInput").clear();
-                $(".react-datepicker__month-select").selectOption(month);
-                $(".react-datepicker__year-select").selectOption(year);
-                $(".react-datepicker__day--0" + day).click();
-            });
-            step("Set subjects", () -> {
-                $("#subjectsInput").val(subjects);
-                $("#subjectsInput").setValue(subjects).pressEnter();
-            });
-            step("Set hobbies", () -> {
-                $("#hobbiesWrapper").$(byText(hobbies)).click();
-            });
-            step("Upload image", () ->
-                    $("#uploadPicture").uploadFromClasspath(picture));
-            step("Set address", () -> {
-                $("#currentAddress").val(address);
-                $("#state").scrollTo().click();
-                $("#stateCity-wrapper").$(byText(state)).click();
-                $("#city").click();
-                $("#stateCity-wrapper").$(byText(city)).click();
-            });
-            step("Submit form", () ->
-                    $("#submit").click());
+            registrationPage.typeFirstName(firstName)
+                    .typeLastName(lastName)
+                    .typeEmail(email)
+                    .selectGender(gender)
+                    .typePhone(phone)
+                    .setDateOfBirth(day, month, year)
+                    .typeSubjects(subjects)
+                    .typeHobbies(hobbies)
+                    .uploudFile(picture)
+                    .typeAdress(address)
+                    .selectState(state)
+                    .selectCity(city);
+            registrationPage.clickButton();
         });
-
         step("Verify successful form submit", () -> {
-            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-            $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
-            $x("//td[text()='Student Email']").parent().shouldHave(text(email));
-            $x("//td[text()='Gender']").parent().shouldHave(text(gender));
-            $x("//td[text()='Mobile']").parent().shouldHave(text(phone));
-            $x("//td[text()='Date of Birth']").parent().shouldHave(text(day + " " + month + "," + year));
-            $x("//td[text()='Subjects']").parent().shouldHave(text(subjects));
-            $x("//td[text()='Hobbies']").parent().shouldHave(text(hobbies));
-            $x("//td[text()='Picture']").parent().shouldHave(text(picture));
-            $x("//td[text()='Address']").parent().shouldHave(text(address));
-            $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
+            registrationPage.checkResultsTitle();
+            registrationPage.checkResultsValue(firstName + " " + lastName)
+                    .checkResultsValue(email)
+                    .checkResultsValue(gender)
+                    .checkResultsValue(phone)
+                    .checkResultsValue(day + " " + month + "," + year)
+                    .checkResultsValue(subjects)
+                    .checkResultsValue(hobbies)
+                    .checkResultsValue(picture)
+                    .checkResultsValue(address)
+                    .checkResultsValue(state + " " + city);
         });
-
-
-
     }
-
 }

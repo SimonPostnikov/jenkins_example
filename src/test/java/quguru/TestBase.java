5aa66eb4203163ecbd3d1e1dde628f314ccfee9a
Simon Postnikov <postnikov.sn@mail.ru>
2021-08-06 16:03:51 +0300

    added library owner

--- src/test/java/quguru/TestBase.java
+++ src/test/java/quguru/TestBase.java
@@ -1,15 +1,21 @@
 package quguru;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Credentials;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import helpers.Attach;
+import static config.Credentials.credentials;
+import static java.lang.String.format;
 
 
 public class TestBase {
+    private static Object Credentials;
+
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
@@ -23,8 +29,10 @@ public class TestBase {
         //Макс браузер
         Configuration.startMaximized = true;
         //Запуск браузера в селеноиде
-        Configuration.remote = "https://user1:1234@" + System.getProperty("url");;
-    }
+        String login = credentials.login();
+        String password =credentials.password();
+        Configuration.remote = format("https://%s:%s@",login,password) + System.getProperty("url");
+    };
 
     @AfterEach
     public void tearDown() {
